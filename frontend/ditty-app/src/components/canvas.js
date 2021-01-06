a9521a1d84a25db1a5569236d9f5238b2fbf137b
Nick <nfoote@utas.edu.au>
2021-01-06 15:32:50 +1100

    add ability to use ml5 library on current image displayed in the gallery

--- frontend/ditty-app/src/components/canvas.js
+++ frontend/ditty-app/src/components/canvas.js
@@ -1,9 +1,26 @@
 import React, { useRef, useEffect, useState } from 'react';
+import * as ml5 from 'ml5';
+import { map } from 'lodash';
 
 const Canvas = ({ imageData }) => {
   const canvasRef = useRef(null);
   const [context, setContext] = useState(null);
+  const [predictions, setPredictions] = useState([]);
   const img = new Image();
+
+  const transformResultData = (results) => map(results, (result) => result.label);
+
+  const classifyImg = () => {
+    // Initialize the Image Classifier method with MobileNet
+    // eslint-disable-next-line no-console
+    const classifier = ml5.imageClassifier('MobileNet', () => { console.log('Model Loaded'); });
+    // Put the image to classify inside a variable
+    const image = img;
+    // Make a prediction with a selected image
+    classifier.predict(image, 5, (err, results) => {
+      setPredictions(transformResultData(results));
+    });
+  };
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -40,6 +57,25 @@ const Canvas = ({ imageData }) => {
           marginTop: 10,
         }}
       />
+      <button
+        type="button"
+        onClick={() => {
+          classifyImg();
+        }}
+      >
+        Classify
+      </button>
+      <div>
+        {predictions.map((p, key) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <p key={key}>
+            {key + 1}
+            :
+            {p}
+          </p>
+        ))}
+      </div>
+
     </div>
   );
 };
